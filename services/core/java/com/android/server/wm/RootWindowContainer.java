5b71c75a1d243d3412e674f7be20ff79dfff46d4
tanxiaoyan <tanxiaoyan@xiaomi.com>
2024-08-14 17:24:50 +0800

    Should prepare surfaces for window which need resize
    Bug:359766320
    
    Change-Id: I20b0440ad7195745cdf1ec2cb6164fea2b69654a
    Signed-off-by: tanxiaoyan <tanxiaoyan@xiaomi.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/RootWindowContainer.java
+++ services/core/java/com/android/server/wm/RootWindowContainer.java
@@ -1019,6 +1019,7 @@ public class RootWindowContainer extends WindowContainer<DisplayContent>
                 // complete configuration.
                 continue;
             }
+            win.prepareSurfaces();
             win.reportResized();
             mWmService.mResizingWindows.remove(i);
         }
